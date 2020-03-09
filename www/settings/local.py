c168f4f8d40d564754ab8e3c6eee4d94fd7db669
Jon Combe <github@joncombe.net>
2020-03-09 15:43:28 +0700

    Remove local file

--- www/settings/local.py
+++
@@ -1,9 +0,0 @@
-def local_config(SALEBOX):
-    SALEBOX['API'] = {
-        'IP': '103.13.228.184',
-        'URL': 'https://th1.salebox.io',
-        'KEY': '76c787a8-a18a-4815-8203-d8664541eb43',
-        'LICENSE': '2f469fd5-764c-460b-898d-f97040f3080b',
-    }
-    SALEBOX['MISC']['POS_USER_ID'] = 5
-    return SALEBOX
