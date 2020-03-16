62059407d2ca835b2ce2ced0522dd816cd3130db
Jon Combe <github@joncombe.net>
2020-03-16 14:01:32 +0700

    Category list on homepage

--- home/models.py
+++ home/models.py
@@ -3,12 +3,17 @@ from django.db import models
 
 from wagtail.core.models import Page
 
+from saleboxdjango.lib.category import SaleboxCategory
 from saleboxdjango.lib.product import SaleboxProduct
 
 
 class HomePage(Page):
     def get_context(self, request):
         context = super().get_context(request)
+
+        # fetch categories
+        sc = SaleboxCategory()
+        context['categories'] = sc.get_tree()
 
         # fetch products
         sp = SaleboxProduct()
